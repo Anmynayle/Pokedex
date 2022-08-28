fa628db130809283a6ab0f07f6d81f35cc65000a
Anmy Moreno <nayle2701@gmail.com>
2022-08-27 22:43:30 -0400

    start2243

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
+import {Route,Routes} from 'react-router-dom'
+import Home from './components/Home/Home'
+import Pokedex from './components/Pokedex/Pokedex'
+import ProtectedRoutes from './components/ProtectedRoutes/ProtectedRoutes'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <div className="App">
+
+      <Routes>
+
+        <Route path='/' element={<Home/>}/>
+        
+        <Route element={<ProtectedRoutes/>}>
+          <Route path='/pokedex' element={<Pokedex/>}/>
+          <Route path='/pokedex/:id' element={<h2>pokeId</h2>}/>
+        </Route>
+      </Routes>
       
     </div>
   )
